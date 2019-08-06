7da04b68a8abad0f2dee3ece7b235f0923454e9b
Randall <randall@duomai.com>
2019-08-06 14:44:11 +0800

    [add] hello-element with array props

---
+++ src/elements/hello.tsx
@@ -0,0 +1,38 @@
+import { define, WeElement, h } from "omi";
+
+define("hello-element", class extends WeElement {
+  onClick = evt => {
+    // trigger CustomEvent
+    this.fire("abc", { name: "Mc", age: 12 });
+    evt.stopPropagation();
+  };
+
+  //如果需要在 html 里直接使用 <hello-element></hello-element>，必须声明 propTypes
+  static propTypes = {
+    msg: String,
+    list: Array
+  };
+
+  static css = `
+      div {
+        color: red;
+        cursor: pointer;
+      }`;
+  installed() {
+    console.log(this.props);
+  }
+
+  render(props) {
+    return (
+      <div onClick={this.onClick}>
+        Hi {props.msg}
+        <div>Click Me!</div>
+        {props.list.map(item => (
+          <a href={item.url} target="_blank">
+            {item.name}
+          </a>
+        ))}
+      </div>
+    );
+  }
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,47 @@
-import { WeElement, render, h, define, tag } from 'omi'
-import './hello-omi'
-import './index.css'
-import * as css from './_index.less'
-import * as logo from './logo.svg'
-import '@omim/core/text-field'
+import { WeElement, render, h, define, tag } from "omi";
+import "./hello-omi";
+import "./index.css";
+import * as css from "./_index.less";
+import * as logo from "./logo.svg";
+import "@omim/core/text-field";
+import "./elements/hello";
 
 interface MyAppProps {
-	name: string
+  name: string;
 }
 
 interface MyAppData {
-	abc: string
+  abc: string;
+  urlList: any;
 }
 
-@tag('my-app')
+@tag("my-app")
 export default class extends WeElement<MyAppProps, MyAppData> {
+  static css = css;
+  constructor() {
+    super();
+    this.data.urlList = [{ name: "test", url: "https://mccarthey.top" }];
+  }
 
-	static css = css
+  onAbc = (evt: CustomEvent) => {
+    this.data.abc = ` by ${evt.detail.name}`;
+    this.update();
+  };
 
-	onAbc = (evt: CustomEvent) => {
-		this.data.abc = ` by ${evt.detail.name}`
-		this.update()
-	}
-
-	render(props) {
-		return (
-			<div class="app">
-				<header class="app-header">
-					<img
-						src={logo}
-						class="app-logo"
-						alt="logo"
-					/>
-					<h1 class="app-title">Welcome to {props.name}</h1>
-				</header>
-				{this.data.abc}
-				<hello-omi onAbc={this.onAbc} msg="Omi"></hello-omi>
-
-				<m-text-field label='test'></m-text-field>
-			</div>
-		)
-	}
+  render(props) {
+    return (
+      <div class="app">
+        <header class="app-header">
+          <img src={logo} class="app-logo" alt="logo" />
+          <h1 class="app-title">Welcome to {props.name}</h1>
+        </header>
+        {this.data.abc}
+        <hello-omi onAbc={this.onAbc} msg="Omi" />
+        <hello-element onAbc={this.onAbc} msg="WeElement" list={this.data.urlList} />
+        <m-text-field label="test" />
+      </div>
+    );
+  }
 }
 
-render(<my-app name='Omi'></my-app>, '#root')
+render(<my-app name="Omi" />, "#root");
